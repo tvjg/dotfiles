a134502005e3856c80bc58ae65a5a0645012e98c
John Guidry <lowdowndirtyrottenscoundrel@gmail.com>
2015-07-24 00:21:37 -0500

    Highlight the current line, reuse dired buffers

--- .emacs.d/customizations/60-custom.el
+++ .emacs.d/customizations/60-custom.el
@@ -5,12 +5,18 @@
 (setq scroll-step 1)
 (setq scroll-conservatively 5)
 
+; Highlight current line
+(global-hl-line-mode t)
+
 ; Tmux causes issues with Ctrl+Arrows
 ; use in combination with xterm-keys in .tmux.conf to restore desired behavior
 (define-key input-decode-map "\e[1;5A" [C-up])
 (define-key input-decode-map "\e[1;5B" [C-down])
 (define-key input-decode-map "\e[1;5C" [C-right])
 (define-key input-decode-map "\e[1;5D" [C-left])
+
+; Use `a` in dired to reuse buffer
+(put 'dired-find-alternate-file 'disabled nil)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
