ca022ba228435cdfe02cc2a82df19be1f8908cd3
John Guidry <lowdowndirtyrottenscoundrel@gmail.com>
2017-03-19 20:31:21 -0500

    Disable missing semicolon warnings in js2-mode

--- emacs.d/lisp/20-javascript.el
+++ emacs.d/lisp/20-javascript.el
@@ -5,6 +5,8 @@
     (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
     (setq js2-highlight-level 3)
+    (setq js2-strict-missing-semi-warning nil)
+    (setq js2-missing-semi-one-line-override nil)
 
     ;; Use plain old js-mode for JSON, js2-mode doth protest too much
     ;; https://github.com/bodil/emacs.d/blob/133386b9cc773377b36fce990d17367438a8916a/bodil/bodil-js.el#L38
