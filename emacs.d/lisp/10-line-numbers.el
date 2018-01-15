c0e2c61d7302d5577677430c02b6271188898eae
John Guidry <lowdowndirtyrottenscoundrel@gmail.com>
2018-01-15 16:29:30 -0600

    Replace relative-line-numbers package
    relative-line-numbers appears to have disppeared off the face of the
    planet. Use nlinum-relative to cover for now.

--- emacs.d/lisp/10-line-numbers.el
+++ emacs.d/lisp/10-line-numbers.el
@@ -1,13 +1,14 @@
-(use-package relative-line-numbers
+(use-package nlinum-relative
   :ensure t
+  :init
+  (add-hook 'prog-mode-hook 'nlinum-relative-mode)
   :config
   (progn
     (line-number-mode t)
     (column-number-mode t)
 
-    (setq relative-line-numbers-format #'padded-line-numbers)
-
-    (defun padded-line-numbers (offset)
-      (format "%2d " (abs offset)))))
+    (nlinum-relative-setup-evil)
+    (setq nlinum-relative-redisplay-delay 0.1)
+    (setq nlinum-relative-current-symbol "0")))
 
 (provide '10-line-numbers)
